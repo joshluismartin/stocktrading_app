be7e9b2e02a109d7c0862114e166901c94b9141b
joshluismartin <joshluis.martin>
2025-07-16 21:47:41 +0800

    feat(core): add controller logic for stocks and trades

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,10 +1,27 @@
 class TradesController < ApplicationController
+  before_action :authenticate_user!
   def new
+    @trade = Trade.new
+    @stocks = Stock.all
   end
 
   def create
+    @trade = current_user.trades.build(trade_params)
+    if @trade.save
+      redirect_to trades_path, notice: "Trade successful!"
+    else
+      render :new
+    end
   end
 
   def index
+    @trades = current_user.trades
+  end
+
+
+  private
+
+  def trade_params
+    params.require(:trade).permit(:stock_id, :trade_type, :quantity, :price)
   end
 end

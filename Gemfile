d2e4f1283dcd20c0bf0efeefc46b1a1262bf0bdc
joshluismartin <joshluis.martin>
2025-07-18 12:54:28 +0800

    feat(api): add AlphaVantage API wrapper with Net::HTTP

--- Gemfile
+++ Gemfile
@@ -19,6 +19,10 @@ gem "jbuilder"
 
 gem "devise"
 
+gem "dotenv-rails"
+
+gem "httparty"
+
 gem "pundit"
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
---
+++ app/wrappers/alpha_api.rb
@@ -0,0 +1,25 @@
+require "uri"
+require "net/http"
+require "json"
+
+class AlphaVantage
+  BASE_URL = "https://alpha-vantage.p.rapidapi.com/query"
+
+  def self.get_stock_price(symbol)
+    url = URI("#{BASE_URL}?datatype=json&output_size=compact&interval=5min&function=TIME_SERIES_INTRADAY&symbol=#{symbol}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-key"] = ENV["RAPIDAPI_KEY"]
+    request["x-rapidapi-host"] = "alpha-vantage.p.rapidapi.com"
+
+    response = http.request(request)
+    if response.code == "200"
+      JSON.parse(response.body)
+    else
+      { "error" => "API request failed with code #{response.code}" }
+    end
+  end
+end

7aff1a4bc9399432a105c6248f26af287b7d0a41
joshluismartin <joshluis.martin>
2025-07-23 16:42:23 +0800

    Show latest stock price on show page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,12 @@
 class StocksController < ApplicationController
   def index
     @stocks = Stock.all
+    @latest_prices = {}
+
+    @stocks.each do |stock|
+      stock_data = AlphaVantage.get_stock_price(stock.symbol)
+      @latest_prices[stock.id] = extract_latest_price(stock_data)
+    end
   end
 
   def show
